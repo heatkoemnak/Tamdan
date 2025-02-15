89db073818e0861987cc9726680e8de4543a1e27
Heat Koemnak <heatkimnak@gmail.com>
2025-02-15 16:10:22 +0700

    changed client URI

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ dotenv.config();
 await connectDB();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://tamdan.vercel.app', credentials: true }));
 
 app.use('/api/register', register);
 app.use('/api/login', login);
