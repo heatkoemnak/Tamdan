e0dce0b8e02e414264c1bacc0f6772d67e5c412a
Heat Koemnak <heatkimnak@gmail.com>
2025-02-17 03:12:55 +0700

    updated

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 // app.use('/api/auth', authRouter);
-app.use('/login', loginLimiter, login);
+app.post('/api/auth/login', loginLimiter, login);
 app.get('/', async (req, res, next) => {
   try {
     res.status(200).json({ message: 'Server is running' });
